6bcae217f666fdcb601020b99a900d53cb0b0824
bm moon <gomi6612@gmail.com>
2024-10-30 17:31:12 +0900

    문병무: 엔진에 러스티 삽입

--- ContentsProject/EduContentsCore.cpp
+++ ContentsProject/EduContentsCore.cpp
@@ -60,11 +60,16 @@ void EduContentsCore::BeginPlay()
 	
 
 
-
-	UEngineAPICore::GetCore()->GetMainWindow().SetWindowTitle("EduWindow");
+	UEngineAPICore::GetCore()->GetMainWindow().SetWindowTitle(UEngineAPICore::GetCore()->GetMainWindow().GetWindowHandle(), "EduWindow");
 
 	// 이거 꼭 호출해줘야 합니다.
-	UEngineAPICore::GetCore()->GetMainWindow().SetWindowPosAndScale({ 0, 0 }, {1280, 720});
+	UEngineAPICore::GetCore()->GetMainWindow().SetWindowPosAndScale({ 0, (ScreenY - (ScreenY / 3)) - 10 - 30 }, { WS_SYSMENU, ScreenY / 3 });
+
+	UEngineAPICore::GetCore()->GetMainWindow().SetWindowAlpha();
+
+	UEngineAPICore::GetCore()->GetSubWindow().SetWindowTitle(UEngineAPICore::GetCore()->GetMainWindow().GetWindowHandleSub(), "SubWindow");
+
+	UEngineAPICore::GetCore()->GetSubWindow().SetWindowPosAndScale({ 0, 3 / ScreenY }, { WS_SYSMENU, (ScreenY - (ScreenY / 3)) });
 
 	UEngineAPICore::GetCore()->CreateLevel<APlayGameMode, APlayer>("Play");
 	//UEngineAPICore::GetCore()->CreateLevel("End");
