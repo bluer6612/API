d23c2ae5bfcd0438bf34e8ad3d6d41d13f8f288f
bm moon <gomi6612@gmail.com>
2024-10-30 18:49:56 +0900

    문병무: 러스티 핸들 나누기 1차 업데이트

--- EnginePlatform/EngineWindow.h
+++ EnginePlatform/EngineWindow.h
@@ -64,7 +64,7 @@ public:
 	// 실력이 낮을수록 남과 공유하려고 안한다.
 	// 자기만 아는걸 대명사처럼 말하고.
 
-	inline void SetWindowTitle(HWND _WindowHandle, std::string_view Text)
+	inline void SetWindowTitle(std::string_view Text)
 	{
 		// WindowAPI의 규칙 이해하고 있느냐.
 		// 전부다 에디터
@@ -72,14 +72,14 @@ public:
 		// 이러한 winapi나 std의 혹은 필수 라이브러의 함수들을 
 		// 랩핑함수가 아니라 
 		// 네이티브 함수라고 합니다.
-		SetWindowTextA(_WindowHandle, Text.data());
+		SetWindowTextA(WindowHandle, Text.data());
 	}
 
 	void SetWindowPosAndScale(FVector2D _Pos, FVector2D _Scale);
 
 	void SetWindowTitleDelete();
 
-	void SetWindowAlpha();
+	void SetWindowAlpha(HWND _WindowHandle);
 
 
 protected:
