9bd512a96fd459a40021183e59fe65d70fcc893e
bm moon <gomi6612@gmail.com>
2024-11-15 15:35:31 +0900

    문병무: 패널 농장 슬롯 업데이트

--- ContentsProject/MenuPanelUI.cpp
+++ ContentsProject/MenuPanelUI.cpp
@@ -20,22 +20,33 @@ AMenuPanelUI::AMenuPanelUI()
 
 	{
 		Sprite[index] = CreateDefaultSubObject<USpriteRenderer>();
-		Sprite[index]->SetComponentCrate(Sprite[index], "002_MenuPanel.png", { 1, 1 }, { Location.X, Location.Y }, ERenderOrder::UI);
+		Sprite[index]->SetComponentCrate(Sprite[index], "002_MenuPanel.png", { 1, 1 }, { Location.X + 1, Location.Y - 1 }, ERenderOrder::UI);
 		++index;
 	}
 
 	{
 		Sprite[index] = CreateDefaultSubObject<USpriteRenderer>();
-		Sprite[index]->SetComponentCrate(Sprite[index], "003_MenuPanel.png", {}, { Location.X + 1, (Location.Y + 126) }, ERenderOrder::UI);
+		Sprite[index]->SetComponentCrate(Sprite[index], "003_MenuPanel.png", {}, { Location.X + 1, (Location.Y + 126) }, ERenderOrder::UIUP);
 		++index;
 	}
 
 	{
-		for (size_t i = 0; i < 44; i++)
+		int Loc = 0;
+		for (size_t i = 0; i < 44; ++i)
 		{
 			SpriteRFarmSlot[i] = CreateDefaultSubObject<USpriteRenderer>();
-			SpriteRFarmSlot[i]->SetComponentCrate(SpriteRFarmSlot[i], "000_crop-seed-button.png", {}, { Location.X + 1, (Location.Y + 126) }, ERenderOrder::UI);
-			++i;
+			SpriteRFarmSlot[i]->SetComponentCrate(SpriteRFarmSlot[i], "000_crop-seed-button.png", {}, { (Location.X - 153 + (Loc * 104)), (Location.Y - 120 + ((Loc / 4) + (i / 4)) * 46)  }, ERenderOrder::UI);
+
+			if (i > 24 - 1)
+			{
+				SpriteRFarmSlot[i]->SetActive(false);
+			}
+
+			++Loc;
+			if (4 <= Loc)
+			{
+				Loc = 0;
+			}
 		}
 	}
 }
